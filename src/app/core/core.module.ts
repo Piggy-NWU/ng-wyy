2618ae266a146b6c443598485e1522abc3bdff03
zxw <IncrediblePiggy@outlook.com>
2022-02-13 13:36:32 +0800

    代码模块化

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,41 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ShareModule } from '../share/share.module'; 
+import { PagesModule } from '../pages/pages.module'; 
+import { ServicesModule } from './../services/services.module';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule, Optional, SkipSelf } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "../app-routing.module";
+import zh from '@angular/common/locales/zh';
+import { registerLocaleData } from '@angular/common';
+import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
 
 
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ServicesModule,
+    PagesModule,
+    ShareModule,
+    AppRoutingModule
+  ],
+  exports:[
+    ShareModule,
+    AppRoutingModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule : CoreModule){
+    if(parentModule){
+      throw new Error('coremodule只能被appmodule引用')
+    }
+
+  }
+}
