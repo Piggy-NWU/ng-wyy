58e0f9b952d7d3f7090364a6c62f699188007154
zxw <IncrediblePiggy@outlook.com>
2022-04-18 00:42:11 +0800

    添加了轮播组件两侧滚动的功能。 新设了组件检测策略ChangeDetectionStrategy.OnPush

--- src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
+++ src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
@@ -1,12 +1,17 @@
-import {Component, Input, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-wy-carousel',
   templateUrl: './wy-carousel.component.html',
-  styleUrls: ['./wy-carousel.component.less']
+  styleUrls: ['./wy-carousel.component.less'],
+  // 组件的变更检测策略。 angular默认一个组件发生变化的时候，会将
+  // 这个组件的子组件和父组件一起检测是否发生变化。 OnPush只有
+  // 在组件的输入属性发生变化，该组件才会检测。 否则其他组件的变化不会引起该组件的检测。有利于提高性能。
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WyCarouselComponent implements OnInit {
   @Input() activeIndex = 0;
+  @Output() changeSlide = new EventEmitter<string>();
   @ViewChild('dot', {static: true})
   dotRef: TemplateRef<any>;
 
@@ -14,6 +19,10 @@ export class WyCarouselComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onChangeSlide(type: string) {
+    this.changeSlide.emit(type);
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Banner} from './../../services/data-types/common.types';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HomeService} from 'src/app/services/home.service';
+import {NzCarouselComponent} from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ export class HomeComponent implements OnInit {
   carouselActiveIndex = 0;
   public banners: Banner[];
 
+  @ViewChild(NzCarouselComponent, {static: true}) private nzCarousel: NzCarouselComponent;
   constructor(private homeServe: HomeService) {
     this.homeServe.getBanners().subscribe(banners => {
       console.log(banners);
@@ -18,10 +20,15 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  OnbeforeChange({to}) {
+  onbeforeChange({to}) {
     this.carouselActiveIndex = to;
   }
 
   ngOnInit() {
+  }
+
+  onChangeSlide(type: string) {
+    // 我靠，angular还可以这么写啊
+    this.nzCarousel[type]();
   }
 }
